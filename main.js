f881a3d6a9b1f1e62f226ebea1b4d74d46bc14a5
KenTran91 <55103563+KenTran91@users.noreply.github.com>
2020-01-23 15:54:21 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+var div1= document.getElementById(`signin`);
+div1.addEventListener(`click`,function(){
+    document.getElementById(`modal`).style.display=`block`;
+})
+var div2= document.getElementById(`close`);
+div2.addEventListener(`click`,function(){
+    document.getElementById(`modal`).style.display=`none`;
+})
+
+
+
